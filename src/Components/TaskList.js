0b2fd8db4d059e7e04e02ffe48f8262b4ae6c9f6
Tony Wang <wangh9947@gmail.com>
2023-07-24 12:54:49 +0800

    onchange checkbox

--- src/Components/TaskList.js
+++ src/Components/TaskList.js
@@ -1,11 +1,21 @@
+import _ from "lodash";
 const TaskList = (props) => {
   const { i, tasks, setTasks } = props;
+  const onChangeHandler = () => {
+    const copy = _.cloneDeep(tasks);
+    copy[i].isComplete = !copy[i].isComplete;
+    setTasks(copy);
+  };
   return (
     <div>
       {tasks.map((e, i) => {
         return (
           <div key={i}>
-            <input type="checkbox" checked={tasks[i].isComplete} />
+            <input
+              type="checkbox"
+              checked={tasks[i].isComplete}
+              onChange={onChangeHandler}
+            />
             {tasks[i].text}
           </div>
         );
